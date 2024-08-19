1a51a2c95a832ec7075450a586492935190464f5
Frane <franezunic7@gmail.com>
2024-08-19 08:27:18 +0200

    bugfix

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ Route::get('/', function () {
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
-        'auth' => auth()->user(),
         'appVersion' => Config::get('app.version'),
     ]);
 });
